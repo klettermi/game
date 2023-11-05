8109fee3a6f81b3cd7e47e5d5c43f1310fb79900
Mi <mi_aa_@naver.com>
2023-11-05 18:20:00 +0900

    feat: 랜덤 숫자 중복 제거

--- src/main/java/com/example/game/global/util/RandomUtils.java
+++ src/main/java/com/example/game/global/util/RandomUtils.java
@@ -1,16 +1,20 @@
 package com.example.game.global.util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class RandomUtils {
     public static int[] setRandomNumber(int size){
         ArrayList<Integer> numArray = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
         while(numArray.size() < size){
             int num = (int) (Math.random() * 9 + 1);
-            numArray.add(num);
+            if(!set.contains(num)) {
+                numArray.add(num);
+                set.add(num);
+            }
         }
 
-        int[] nums = numArray.stream().mapToInt(i -> i).toArray();
-        return nums;
+        return numArray.stream().mapToInt(i -> i).toArray();
     }
 }
